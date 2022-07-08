3025b57904ee74fcc390f086cf9a925ccd26ad83
github-140952 <40254866+github-140952@users.noreply.github.com>
2022-07-08 12:44:25 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "name" {
+    type = string
+    default = "moduleserver"
+}
+variable "dept" {
+    type = string
+    default = "IT"
+}
+variable "bucket_name" {
+    type = string
+    default = "taxes"
+}
